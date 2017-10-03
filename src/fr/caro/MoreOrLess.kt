d3428fd21b132f3acf5e47ace43e1265a90a3fe9
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-10-03 11:13:05 +0200

    custom min and max

--- src/fr/caro/MoreOrLess.kt
+++ src/fr/caro/MoreOrLess.kt
@@ -1,14 +1,20 @@
 package fr.caro
 
 import java.util.*
+import kotlin.system.exitProcess
 
 /**
- * Created by caroline on 02/09/17.
+ * author : Caroline Chaudey
+ * created : 02/09/2017
+ * modified : 03/10/2017
  */
 
 fun main(args: Array<String>) {
 
-    val goal = chooseANumber()
+    val pair = getMinMax(args)
+    val goal = if (pair == null) randomNumberPicker()
+               else randomNumberPicker(pair.first, pair.second)
+
     var nbAttempts = 0
     do {
         nbAttempts++
@@ -18,7 +24,7 @@ fun main(args: Array<String>) {
     "Congratulation ! You found in $nbAttempts attemps.".outputAsComputer()
 }
 
-fun chooseANumber(min: Int = 0, max: Int = 100): Int {
+fun randomNumberPicker(min: Int = 0, max: Int = 100): Int {
     val random = SplittableRandom()
     val goal = random.nextInt(min, max)
     "I chose a number between $min and $max".outputAsComputer()
@@ -33,6 +39,27 @@ fun tryingToGuess(goal: Int) : Int {
         userGuess < goal -> "more !".outputAsComputer()
     }
     return userGuess
+}
+
+fun getMinMax(args: Array<String>) : Pair<Int, Int>? {
+    when {
+        args.isEmpty() -> return null
+        args.size == 1 -> closeApp("You must provide the minimum AND maximum value. Or nothing. Not just one argument.")
+    }
+    try {
+        val min = args[0].toInt()
+        val max = args[1].toInt()
+        if (min >= max) closeApp("The max value must be above the minimum value.")
+        return Pair(min, max)
+    } catch (e: NumberFormatException) {
+        closeApp("The first 2 arguments must be integers.")
+    }
+    return null
+}
+
+fun closeApp(message: String = "Error. Closing the app.") {
+    println(message)
+    exitProcess(0)
 }
 
 fun String.outputAsComputer() {
